3d3681510aa0c2406b6ee8ac29b23096b86cb525
Nyasha Karata <164152028+odlemon@users.noreply.github.com>
2025-03-06 08:40:52 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'https://ihr-demo.vercel.app',
-  'https://ihr-demo-lilac.vercel.app/log-in',
+  'https://ihr-demo-lilac.vercel.app',
 ];
 
 app.use(
